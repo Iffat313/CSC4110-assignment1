7dee4082960747ff0a5ca49a5980687af883dc1a
Iffat Rahman <iffatrahman99@gmail.com>
2025-01-23 00:16:07 -0500

    Adding Author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,5 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Iffat Rahman <iffatrahman99@gmail.com>
 // Header file for input output functions 
 #include<iostream>  
   
